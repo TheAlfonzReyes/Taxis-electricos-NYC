6de75e9c5932c83540fb06f32037892ac103cbfc
Nestor Saenz <nes164@gmail.com>
2024-03-07 11:15:01 -0500

    imagenes y creacion de carpeta cloud functions

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ async def my_function():
     return 'PROYECTO INDIVIDUAL Nº1 Machine Learning Operations (MLOps)'
 
 @app.get('/')
-def determinar_efectivo():
-    
+def determinar_efectivo_():
+    df = pd.read_parquet("C:\Users\Acer\Downloads\green_tripdata_2023-12.parquet", engine='fastparquet')
+    tar_credito = round((df[df['modalidad_pago'] == 1].count()/(df.shape[0])*100).reset_index(), 2)
+    tar_credito.iloc[17, :].values[1]
